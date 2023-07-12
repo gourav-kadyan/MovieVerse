d604387657d9ba379be036f71886f7e126a2fc20
Gourav Kadyan <69764718+gourav-kadyan@users.noreply.github.com>
2023-07-12 13:14:32 +0530

    update cards css

--- src/Component/Cards.jsx
+++ src/Component/Cards.jsx
@@ -31,7 +31,7 @@ const Cards = () => {
               key={i}
               className="card shadow-lg p-2 cursor-pointer hover:-translate-y-3 font-bold mt-6 transition-all duration-500"
             >
-              <img className="h-60 md:h-72" src={e.image} alt="movie poster" />
+              <img className="h-60 md:h-72 w-40 md:w-48 md:48" src={e.image} alt="movie poster" />
               <h1>
                 <span className="text-gray-500">Name : </span> {e.title}
               </h1>
